3a50dbf9335c334c4613a7e0467651d3b9afe932
juliet-264 <136452043+juliet-264@users.noreply.github.com>
2024-07-17 12:35:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,4 @@
-// let button = document.querySelector('button')
-// button.addEventListener('click',)
+let button = document.querySelector('button')
+button.addEventListener('click',)
 
 
-let item ={
-    name :`sound`,
-    age : 22
-
-}
-console.log(item.name)
-
-let ob = new Object()
-
-ob.price = 1000
-ob.name ='santu'
-console.log(ob)
